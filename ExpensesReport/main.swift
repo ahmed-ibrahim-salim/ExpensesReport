150efd119639ef36a9a4d0596a94190ed982bde7
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2023-11-14 23:36:38 +0200

    update: write full test suit for original code with simple refactor

--- ExpensesReport/main.swift
+++ ExpensesReport/main.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 
+// New Feature: Add Lunch with an expense limit of 2000.
+
+// Change
 enum ExpenseType {
     case breakfast
     case dinner
@@ -18,44 +21,62 @@ struct Expense {
     var amount: Int
 }
 
-class ExpenseReport {
-    func printReport(expenses: [Expense]) {
+// Change
+final class ExpenseReporter {
+    // Change
+    func makeReport(expenses: [Expense]) -> String {
         var mealExpenses = 0
         var total = 0
-        print("Expense Report \(Date())")
+        var report = ""
+        report += "Expense Report \(Date())\n"
         for expense in expenses {
+            // Change
             if (expense.type == .dinner || expense.type == .breakfast) {
                 mealExpenses += expense.amount
             }
 
-            var expenseName = ""
-            switch expense.type {
-            case .breakfast: expenseName = "Breakfast"
-            case .dinner: expenseName = "Dinner"
-            case .carRental: expenseName = "Car Rental"
-            }
+            let expenseName = getExpenseName(expense)
+            let mealOverExpensesMarker = getMealOverExpensesMarker(expense)
 
-            let mealOverExpensesMarker = expense.type == .dinner && expense.amount > 5000 || expense.type == .breakfast && expense.amount > 1000 ? "X" : " "
-            print("\(expenseName)\t\(expense.amount)\t\(mealOverExpensesMarker)")
+            report += "\(expenseName)\t\(expense.amount)\t\(mealOverExpensesMarker)\n"
 
             total += expense.amount
         }
-        print("Meal Expenses: \(mealExpenses)")
-        print("Total Expenses: \(total)")
+        report += "Meal Expenses: \(mealExpenses)\n"
+        report += "Total Expenses: \(total)"
+
+        return report
+    }
+
+    func getExpenseName(_ expense: Expense) -> String {
+        switch expense.type {
+        case .breakfast: return "Breakfast"
+        case .dinner: return "Dinner"
+        case .carRental: return "Car Rental"
+        }
+    }
+
+    func getMealOverExpensesMarker(_ expense: Expense)->String{
+        // Change
+        let isDinnerExpensive = expense.type == .dinner && expense.amount > 5000
+        let isBreakfastExpensive = expense.type == .breakfast && expense.amount > 1000
+        return isDinnerExpensive || isBreakfastExpensive ? "X" : ""
     }
 }
 
-let report = ExpenseReport()
-report.printReport(
+let reporter = ExpenseReporter()
+let report = reporter.makeReport(
     expenses: [
-        .init(type: .breakfast, amount: 10),
-        .init(type: .dinner, amount: 20),
+        .init(type: .breakfast, amount: 10000),
         .init(type: .dinner, amount: 20),
         .init(type: .carRental, amount: 100)
     ]
 )
+print(report)
 /*
  1 - long method
  2 - OCP voilation
  3 - primitive obsession
+ 4 - long one line if conditions
+ 5 - cannot be tested
  */
--- ExpensesReportTest/ExpensesReportTest.swift
+++ ExpensesReportTest/ExpensesReportTest.swift
@@ -6,6 +6,16 @@
 //
 
 import XCTest
+//@testable import ExpensesReport
+
+/*
+ Expense Report 2023-11-14 21:02:59 +0000
+ Breakfast 10000 X
+ Dinner 20
+ Car Rental 100
+ Meal Expenses: 10040
+ Total Expenses: 10140
+ */
 
 final class ExpensesReportTest: XCTestCase {
 
@@ -17,9 +27,128 @@ final class ExpensesReportTest: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        XCTFail()
+    /*
+     Expense Report 2023-11-14 21:02:59 +0000
+     Meal Expenses: 0
+     Total Expenses: 0
+     */
+    func test_makePrint_whenNoExpenses_returnEmptyReport() throws {
+        let expected = """
+        Expense Report \(Date())
+        Meal Expenses: 0
+        Total Expenses: 0
+        """
+        // given
+        let report = ExpenseReporter()
+        let expenses: [Expense] = []
+
+        // when
+        let actualReport = report.makeReport(expenses: expenses)
+
+        // then
+        XCTAssertEqual(actualReport, expected)
     }
 
+    /*
+     Expense Report 2023-11-14 21:02:59 +0000
+     Breakfast 10
+     Dinner 20
+     Car Rental 100
+     Meal Expenses: 30
+     Total Expenses: 130
+     */
+    func test_makePrint_whenhasExpenses_returnReportCorrectFullReport() throws {
+        var expected = "Expense Report \(Date())\n"
+        expected += "Breakfast\t10\t\n"
+        expected += "Dinner\t20\t\n"
+        expected += "Car Rental\t100\t\n"
+        expected += "Meal Expenses: 30\n"
+        expected += "Total Expenses: 130"
 
+        // given
+        let report = ExpenseReporter()
+        let expenses: [Expense] = [
+            .init(type: .breakfast, amount: 10),
+            .init(type: .dinner, amount: 20),
+            .init(type: .carRental, amount: 100)
+        ]
+
+        // when
+        let actualReport = report.makeReport(expenses: expenses)
+
+        // then
+        XCTAssertEqual(actualReport, expected)
+    }
+
+    /*
+     Expense Report 2023-11-14 21:02:59 +0000
+     Breakfast 2000 X
+     Meal Expenses: 2000
+     Total Expenses: 2000
+     */
+    func test_makePrint_whenBreakfastIsExpensive_returnReportBreakfastMarked() throws {
+        var expected = "Expense Report \(Date())\n"
+        expected += "Breakfast\t2000\tX\n"
+        expected += "Meal Expenses: 2000\n"
+        expected += "Total Expenses: 2000"
+        // given
+        let report = ExpenseReporter()
+        let expenses: [Expense] = [
+            .init(type: .breakfast, amount: 2000)
+        ]
+
+        // when
+        let actualReport = report.makeReport(expenses: expenses)
+
+        // then
+        XCTAssertEqual(actualReport, expected)
+    }
+
+    /*
+     Expense Report 2023-11-14 21:02:59 +0000
+     Dinner 2000 X
+     Meal Expenses: 2000
+     Total Expenses: 2000
+     */
+    func test_makePrint_whenDinnerIsExpensive_returnReportWithDinnerMarked() throws {
+        var expected = "Expense Report \(Date())\n"
+        expected += "Dinner\t7000\tX\n"
+        expected += "Meal Expenses: 7000\n"
+        expected += "Total Expenses: 7000"
+        // given
+        let report = ExpenseReporter()
+        let expenses: [Expense] = [
+            .init(type: .dinner, amount: 7000)
+        ]
+
+        // when
+        let actualReport = report.makeReport(expenses: expenses)
+
+        // then
+        XCTAssertEqual(actualReport, expected)
+    }
+
+    /*
+     Expense Report 2023-11-14 21:02:59 +0000
+     Car Rental 100
+     Meal Expenses: 0
+     Total Expenses: 100
+     */
+    func test_makePrint_whenMealExpense_returnReportOnlyForNoneMealExpense() throws {
+        var expected = "Expense Report \(Date())\n"
+        expected += "Car Rental\t100\t\n"
+        expected += "Meal Expenses: 0\n"
+        expected += "Total Expenses: 100"
+        // given
+        let report = ExpenseReporter()
+        let expenses: [Expense] = [
+            .init(type: .carRental, amount: 100)
+        ]
+
+        // when
+        let actualReport = report.makeReport(expenses: expenses)
+
+        // then
+        XCTAssertEqual(actualReport, expected)
+    }
 }
